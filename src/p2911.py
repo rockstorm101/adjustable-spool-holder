30338d4ffec9ef67cc0af6a61373ec5beccce3e7
Rock Storm <rockstorm@gmx.com>
2021-02-10 21:10:40 +0000

    p2911: Remove sharp edges and add lead-in chamfer

--- src/p2911.py
+++ src/p2911.py
@@ -11,21 +11,34 @@ threads = sp.import_scad('../lib/MCAD/fasteners/threads.scad')
 
 # Config
 dim10 = 80    # OD
-dim11 = bearing.OD + 0.2  # bearing seat ID
-dim12 = 11.8  # bearing seat depth
-dim13 = 15    # full height
+dim11 = bearing.OD + 0.4  # bearing seat ID
+dim13 = 16    # full height
 dim14 = 38.5  # flange ID
+dim15 = 2     # flange height
+dim16 = dim13 - dim15   # bearing seat depth
 
 # Connectors
 DEFAULT_CONN = [[0, 0, 0], [0, 0, -1], 0]
-BEARING_CONN = [[0, 0, dim12], [0, 0, -1], 0]
+BEARING_CONN = [[0, 0, dim16], [0, 0, -1], 0]
 
 @spu.bom_part(description = "Clamp")
 def part(variant = 'A', configuration = 'default', debug = False):
-    tmp = sp.cylinder(h=dim13, d1=dim10, d2=dim14)
-    tmp -= sp.translate([0,0,-0.1])(sp.cylinder(d=dim11, h=dim12+0.1))
-    tmp -= sp.translate([0,0,-0.1])(sp.cylinder(d=dim14, h=dim13+0.2))
+    out_cyl = sp.cylinder(h=dim15+0.001, d=dim10)
+    cone = sp.cylinder(h=dim16, d1=dim10, d2=dim11)
+    cone = sp.translate([0,0,dim15])(cone)
+    tmp = out_cyl + cone
+    bseat = sp.translate([0,0,-0.1])( sp.cylinder(d=dim11, h=dim16+0.1) )
+    tmp -= bseat
+    flange = sp.cylinder(d=dim14, h=dim15+0.2)
+    flange = sp.translate([0,0,dim16-0.1])(flange)
+    tmp -= flange
+    chamfer = threads.chamfer_cylinder(cyl_diameter = dim11,
+                                       chfr_diameter = dim11 + 2*1.5,
+                                       angle = 30,
+                                       internal = True)
+    tmp -= chamfer
     if debug: tmp += assembly.connector(DEFAULT_CONN)
+    if debug: tmp += assembly.connector(BEARING_CONN)
     return tmp
 
 if __name__ == '__main__':
