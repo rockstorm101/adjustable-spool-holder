57e9f13c64eeb9bc65603ae97e480c400e4bc300
Rock Storm <rockstorm@gmx.com>
2021-02-13 10:02:02 +0000

    p2910: Long screw, add argparse and fix clearance

--- src/p2910.py
+++ src/p2910.py
@@ -1,4 +1,5 @@
 # Python imports
+import argparse
 import math
 import solid as sp
 import solid.utils as spu
@@ -14,7 +15,7 @@ dim10 = 14              # first step height
 B_h = bearing.B + 1.4   # bearing seat height
 F_d = 29.5              # flange OD, which the bearing seats against
 B_d = bearing.ID - 0.2  # bearing seat diam
-dim14 = 90              # full length
+dim14 = 110             # full length
 T_d = 20                # thread nom diam
 T_p = 2.5               # thread pitch
 T_c = 0.3               # thread clearance
@@ -26,7 +27,7 @@ BEARING_CONN = [[0, 0, dim10], [0, 0, 1], 0]
 NUT_CONN =     [[0, 0, dim14-20.2], [0, 0, -1], 0]
 
 @spu.bom_part(description = "Screw")
-def part(variant = 'A', configuration = 'default', debug = False):
+def part(version = '', variant = '', configuration = '', debug = False):
     tmp = sp.cylinder(d=F_d, h=dim10)
     tmp += sp.translate([0,0,dim10-0.1])(sp.cylinder(d=B_d, h=B_h+0.1))
     #tmp += sp.translate([0,0,+0.1])(sp.cylinder(d=T_p, h=dim14-0.1))
@@ -35,7 +36,7 @@ def part(variant = 'A', configuration = 'default', debug = False):
                               pitch = T_p,
                               length = dim17,
                               internal = False,
-                              clearance = 1 ),
+                              clearance = T_c ),
         threads.chamfer_cylinder(T_d, T_d - 2*T_p - 0.5, internal = False)
     )
     tmp += sp.translate([0,0,dim17+B_h+dim10])( sp.rotate([180,0,0])(thread) )
@@ -50,25 +51,16 @@ def part(variant = 'A', configuration = 'default', debug = False):
     if debug: tmp += assembly.connector(BEARING_CONN)
     return tmp
 
-def chamfer_cylinder_external(length = 1, angle = 45, diameter = 10):
-    # create a solid chamfer that then has to be substracted
-    #   length = chamfer length
-    #   angle  = chamfer angle
-    #   diameter = diameter of the cylinder onto which this chamfer
-    #              will be applied
-    e = length
-    tg_a = math.tan(math.radians(angle))
-    d = diameter
-    h = e / tg_a
-    e_p = e + 0.1*(1 + tg_a)
-    h_p = h + 0.1*(1 + 1/tg_a)
-    p1 = [d/2 + 0.1 - e_p,      -0.1, 0]
-    p2 = [d/2 + 0.1,            -0.1, 0]
-    p3 = [d/2 + 0.1,       h_p - 0.1, 0]
-    tmp = sp.rotate_extrude()( sp.polygon([p1, p2, p3]) )
-    return tmp
-
 if __name__ == '__main__':
-    sp.scad_render_to_file(part(), include_orig_code = False)
-    # One line per variant to generate all .scad files
-    # sp.scad_render_to_file(part(variant = B), include_orig_code = False)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--debug', action='store_true',
+                        help = "include debug code on SCAD render")
+    parser.add_argument('-e', '--version', default='',
+                        help = "add 'VERSION' to P/N")
+    args = parser.parse_args()
+
+    for variant in ['']:
+        filename = parser.prog.replace(".py","")+args.version+variant
+        tmp = part(variant=variant, version=args.version, debug=args.debug)
+        sp.scad_render_to_file(tmp, filepath=filename+".scad",
+                               include_orig_code = False)
