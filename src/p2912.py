ba58a9e64c5251a3ab1531fae313504a1a8912df
Rock Storm <rockstorm@gmx.com>
2021-02-13 11:01:59 +0000

    p2912: Clean up, add argparse and P/N size/font

--- src/p2912.py
+++ src/p2912.py
@@ -1,4 +1,5 @@
 # Python imports
+import argparse
 import math
 import solid as sp
 import solid.utils as spu
@@ -22,16 +23,17 @@ B_d = screw.B_d  # bearing seat diameter
 B_h = screw.B_h  # bearing seat depth
 N_af = 30        # nut across-flats dimension
 N_h = 10         # nut height
-PN_s = 5         # part number character height
+PN_s = 3         # part number character height
 PN_d = 0.3       # part number engraving depth
 dim14 = F_t + B_h + N_h  # full length
+_part_number = ''
 
 # Connectors
 THREAD_CONN = [[0, 0, 0], [0, 0, 1], 0]
 BEARING_CONN = [[0, 0, F_t+N_h], [0, 0, 1], 0]
 
 @spu.bom_part(description = "Nut")
-def part(ver = "01", variant = 'A', configuration = 'default', debug = False):
+def part(version = '', variant = '', configuration = '', debug = False):
     nut = regular_shapes.hexagon(0,0,across_flats = N_af)
     tmp = sp.linear_extrude(N_h)(nut)
     flange = sp.cylinder(d=F_d, h=F_t+0.1)
@@ -47,34 +49,33 @@ def part(ver = "01", variant = 'A', configuration = 'default', debug = False):
     thread = threads.chamfered_thread(dim14+0.2, internal = True)(
         thread, chamfer, chamfer)
     tmp -= sp.translate([0,0,-0.1])(thread)
-    pn1 = sp.text(text="2912", size = 5, halign = "center")
-    pn2 = sp.text(text = ver + variant, size = 5, halign = "center")
-    tmp -= locate_part_number(pn1)
-    tmp -= sp.rotate([0,0,60])(locate_part_number(pn2))
-    # tmp -= chamfer_cylinder_internal(T_d+0.25)
-    # tmp -= sp.translate([0,0,dim14])(
-    #     sp.rotate([180,0,0])(
-    #         chamfer_cylinder_internal(T_d+0.25) ) )
+    tmp -= locate_part_number(_part_number+version+variant)
+    # tmp -= sp.rotate([0,0,60])(locate_part_number(version+variant))
     if debug: tmp += assembly.connector(BEARING_CONN)
     if debug: tmp += assembly.connector(THREAD_CONN)
     return tmp
 
-def locate_part_number(part_number):
-    pn = part_number
+def locate_part_number(text):
+    pn = sp.text(text=text,
+                 size = PN_s,
+                 font="Open Sans:style=Bold",
+                 halign = "center")
     pn = sp.linear_extrude(PN_d+0.001)(pn)
     pn = sp.rotate([90,0,0])(pn)
     pn = sp.translate([0,-N_af/2+PN_d-0.001,(N_h-PN_s)/2])(pn)
     return pn
 
-def chamfer_cylinder_internal(diameter = 10, angle = 45):
-    d = diameter/2
-    h = d / math.tan(math.radians(angle))
-    h_p = h + 0.1
-    d_p = h_p / h * d
-    tmp = sp.translate([0,0,-0.1])(sp.cylinder(h = h_p, d1 = 2*d_p, d2 = 0))
-    return tmp
-
 if __name__ == '__main__':
-    sp.scad_render_to_file(part(), include_orig_code = False)
-    # One line per variant to generate all .scad files
-    # sp.scad_render_to_file(part(variant = B), include_orig_code = False)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--debug', action='store_true',
+                        help = "include debug code on SCAD render")
+    parser.add_argument('-e', '--version', default='',
+                        help = "add 'VERSION' to P/N")
+    args = parser.parse_args()
+
+    _part_number = parser.prog[1:].replace(".py","")
+    for variant in ['']:
+        filename = parser.prog.replace(".py","")+args.version+variant
+        tmp = part(variant=variant, version=args.version, debug=args.debug)
+        sp.scad_render_to_file(tmp, filepath=filename+".scad",
+                               include_orig_code = False)
