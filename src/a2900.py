e167314aba547e506baecf08032288aa090aff3d
Rock Storm <rockstorm@gmx.com>
2021-02-13 12:39:10 +0000

    a2900: Add argparse and rearrange to include spool

--- src/a2900.py
+++ src/a2900.py
@@ -1,4 +1,5 @@
 # Python imports
+import argparse
 import solid as sp
 import solid.utils as spu
 
@@ -7,33 +8,45 @@ import p2910 as screw
 import p2911 as clamp
 import p2912 as nut
 import v2901 as bearing
+import m2901 as spool
 
 # SCAD imports
 assembly = sp.import_scad('../lib/MCAD/assembly/attach.scad')
 
-def part(variant = 'A', configuration = 'default', debug = False):
+def assy(version = '', variant = '', configuration = '', debug = False):
     C = sp.color("SeaGreen", 1)( clamp.part(debug=debug) )
     B = bearing.part(debug=debug)
     N = sp.color("Plum", 1)( nut.part(debug=debug) )
     S = sp.color("Khaki", 1)(screw.part(debug=debug))
+    P = sp.color("White", 0.5)(spool.part(debug=debug))
 
-    BC = B + assembly.attach(bearing.RACEO_CONN, clamp.BEARING_CONN)(C)
-    NBC = N + assembly.attach(nut.BEARING_CONN, bearing.RACEI_CONN)(BC)
-
-    tmp = S + assembly.attach(screw.BEARING_CONN, bearing.RACEI_CONN)(BC)
-    tmp    += assembly.attach(screw.NUT_CONN, nut.THREAD_CONN)(NBC)
+    tmp = B + assembly.attach(bearing.RACEI_CONN, nut.BEARING_CONN)(N)
+    tmp = C + assembly.attach(clamp.BEARING_CONN, bearing.RACEO_CONN)(tmp)
+    tmp = P + assembly.attach(spool.FWD_CLAMP_CONN, clamp.SPOOL_CONN)(tmp)
+    tmp = C + assembly.attach(clamp.SPOOL_CONN, spool.AFT_CLAMP_CONN)(tmp)
+    tmp = B + assembly.attach(bearing.RACEO_CONN, clamp.BEARING_CONN)(tmp)
+    tmp = S + assembly.attach(screw.BEARING_CONN, bearing.RACEI_CONN)(tmp)
 
     tmp = sp.rotate([0,90,0])(tmp)
-    if debug:
-        tmp -= sp.translate([50-0.1,-50,0])(
-            sp.color("White",0.1)( sp.cube([100,100,100], center = True) )
-        )
+    # if debug:
+    #     tmp -= sp.translate([50-0.1,-50,0])(
+    #         sp.color("White",0.1)( sp.cube([100,100,100], center = True) )
+    #     )
     return tmp
 
 if __name__ == '__main__':
-    tmp = part()
-    sp.scad_render_to_file(tmp, include_orig_code = False)
-    # One line per variant to generate all .scad files
-    # sp.scad_render_to_file(part(variant = B), include_orig_code = False)
-    bom = spu.bill_of_materials(tmp, csv=True)
-    print(bom)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--debug', action='store_true',
+                        help = "include debug code on SCAD render")
+    parser.add_argument('-e', '--version', default = '',
+                        help = "add 'VERSION' to P/N")
+    args = parser.parse_args()
+
+    for variant in ['']:
+        filename = parser.prog.replace(".py","")+args.version+variant
+        tmp = assy(variant=variant, version=args.version, debug=args.debug)
+        sp.scad_render_to_file(tmp, filepath=filename+".scad",
+                               include_orig_code = False)
+        bom = spu.bill_of_materials(tmp, csv=True)
+        with open(filename+"BOM.csv", 'w') as bom_file:
+            bom_file.write(bom)
