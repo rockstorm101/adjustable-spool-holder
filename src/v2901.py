a84ff98692b5b177a701e9ecadc1067d4f6ddf58
Rock Storm <rockstorm@gmx.com>
2021-02-13 11:03:51 +0000

    v2901: Add argparse and debug connector

--- src/v2901.py
+++ src/v2901.py
@@ -1,4 +1,5 @@
 # Python imports
+import argparse
 import solid as sp
 import solid.utils as spu
 
@@ -15,13 +16,23 @@ RACEI_CONN = [[0, 0, 0], [0, 0,  1], 0]
 RACEO_CONN = [[0, 0, B], [0, 0, -1], 0]
 
 @spu.bom_part(description = "Bearing 6905-ZZ")
-def part(variant = 'A', configuration = 'default', debug = False):
+def part(version = '', variant = '', configuration = '', debug = False):
     tmp = sp.cylinder(d=OD, h=B)
     tmp -= sp.translate([0,0,-0.1])(sp.cylinder(d=ID, h=B+0.2))
     if debug: tmp += assembly.connector(RACEI_CONN)
+    if debug: tmp += assembly.connector(RACEO_CONN)
     return tmp
 
 if __name__ == '__main__':
-    sp.scad_render_to_file(part(), include_orig_code = False)
-    # One line per variant to generate all .scad files
-    # sp.scad_render_to_file(part(variant = B), include_orig_code = False)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--debug', action='store_true',
+                        help = "include debug code on SCAD render")
+    parser.add_argument('-e', '--version', default='',
+                        help = "add 'VERSION' to P/N")
+    args = parser.parse_args()
+
+    for variant in ['']:
+        filename = parser.prog.replace(".py","")+args.version+variant
+        tmp = part(variant=variant, version=args.version, debug=args.debug)
+        sp.scad_render_to_file(tmp, filepath=filename+".scad",
+                               include_orig_code = False)
