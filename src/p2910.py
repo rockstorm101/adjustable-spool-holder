da3db1aa54da19ef3061d8c91922c79e33b83318
Rock Storm <rockstorm@gmx.com>
2021-02-24 17:22:52 +0000

    p10: Add lead-in chamfer and P/N

--- src/p2910.py
+++ src/p2910.py
@@ -10,6 +10,7 @@ import v2901 as bearing
 assembly = sp.import_scad('../lib/MCAD/assembly/attach.scad')
 threads = sp.import_scad('../lib/MCAD/fasteners/threads.scad')
 chamfers = sp.import_scad('../lib/MCAD/fillets/chamfers.scad')
+curved_text = sp.import_scad('../lib/MCAD/text/curved_text.scad')
 
 # Config
 dim10 = 14              # first step height
@@ -20,7 +21,10 @@ dim14 = 110             # full length
 T_d = 20                # thread nom diam
 T_p = 2.5               # thread pitch
 T_c = 0.7               # thread clearance
+PN_s = 5                # P/N character height
+PN_d = 0.3              # P/N character depth
 dim17 = dim14 - dim10 - B_h  # thread length
+_code_name = "2910"
 _version = ''
 
 
@@ -28,10 +32,19 @@ _version = ''
 BEARING_CONN = [[0, 0, dim10], [0, 0, 1], 0]
 NUT_CONN =     [[0, 0, dim14-20.2], [0, 0, -1], 0]
 
-@spu.bom_part(description = "Screw", code_name = "2910")
+@spu.bom_part(description = "Screw", code_name = _code_name)
 def part(variant = '', configuration = '', debug = False):
     tmp = sp.cylinder(d=F_d, h=dim10)
-    tmp += sp.translate([0,0,dim10-0.1])(sp.cylinder(d=B_d, h=B_h+0.1))
+    bseat = chamfers.mcad_chamfered_cylinder(B_h+0.001, internal=False)(
+        sp.cylinder(d=B_d, h=B_h+0.001),
+        chamfers.mcad_chamfer_cylinder(diameter = B_d,
+                                       length = None,
+                                       angle = 30,
+                                       depth = 1,
+                                       internal = False)
+    )
+    bseat = sp.translate([0,0,B_h])( sp.rotate([180,0,0])( bseat ) )
+    tmp += sp.translate([0,0,dim10-0.001])( bseat )
     thread = chamfers.mcad_chamfered_cylinder(dim17, internal = False)(
         threads.metric_thread(diameter = T_d,
                               pitch = T_p,
@@ -47,6 +60,16 @@ def part(variant = '', configuration = '', debug = False):
             sp.import_(
                 "../aux/Ender_3_spool_holder/files/" +
                 "Ender3_Spool_Holder_Coupling.stl") ) )
+    pn = curved_text.mcad_cylinder_text(diameter=F_d-PN_d,
+                                        t = _code_name+_version+variant,
+                                        depth = PN_d+0.001,
+                                        size = PN_s,
+                                        font = "Open Sans:style=Bold",
+                                        halign = "center",
+                                        valign = "bot",
+                                        spacing = 1,
+                                        direction = "ccw")
+    tmp -= sp.translate([0,0,dim10/2-PN_s/2])( pn )
     if debug: tmp += assembly.connector(BEARING_CONN)
     return tmp
 
