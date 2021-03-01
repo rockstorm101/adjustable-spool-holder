d01b4a26525430a8be752f96bf0fa34d00747e08
Rock Storm <rockstorm@gmx.com>
2021-03-01 19:01:38 +0000

    p11: Add engraved P/N

--- src/p2911.py
+++ src/p2911.py
@@ -18,6 +18,7 @@ import v2901 as bearing
 # SCAD imports
 assembly = sp.import_scad('../lib/MCAD/assembly/attach.scad')
 chamfers = sp.import_scad('../lib/MCAD/fillets/chamfers.scad')
+curved_text = sp.import_scad('../lib/MCAD/text/curved_text.scad')
 
 # Config
 dim10 = 80    # OD
@@ -26,13 +27,17 @@ dim13 = 16    # full height
 dim14 = 38.5  # flange ID
 dim15 = 2     # flange height
 dim16 = dim13 - dim15   # bearing seat depth
+PN_s = 5                # P/N character height
+PN_d = 0.3              # P/N character depth
+_code_name = "2911"
+_version = ''
 
 # Connectors
 SPOOL_CONN = [[0, 0, dim16-bearing.B/2], [0, 0, 1], 0]
 BEARING_CONN = [[0, 0, dim16], [0, 0, -1], 0]
 
 
-@spu.bom_part(description = "Clamp", code_name = "2911")
+@spu.bom_part(description = "Clamp", code_name = _code_name)
 def part(variant = '', configuration = '', debug = False):
     out_cyl = sp.cylinder(h=dim15+0.001, d=dim10)
     cone = sp.cylinder(h=dim16, d1=dim10, d2=dim11)
@@ -49,6 +54,16 @@ def part(variant = '', configuration = '', debug = False):
                                              depth = None,
                                              internal = True)
     tmp -= chamfer
+    pn = curved_text.mcad_circle_text(diameter=(dim10+dim11)/2-PN_s,
+                                      t = _code_name+_version+variant,
+                                      size = PN_s,
+                                      font = "Open Sans:style=Bold",
+                                      halign = "center",
+                                      valign = "baseline",
+                                      spacing = 1,
+                                      direction = "cw")
+    pn = sp.linear_extrude(PN_d+0.001)( pn )
+    tmp -= sp.translate([0,0,PN_d])( sp.rotate([0,180,0])(pn) )
     if debug: tmp += assembly.connector(SPOOL_CONN)
     if debug: tmp += assembly.connector(BEARING_CONN)
     return tmp
@@ -60,6 +75,7 @@ if __name__ == '__main__':
     parser.add_argument('-e', '--version', default='',
                         help = "add 'VERSION' to P/N")
     args = parser.parse_args()
+    _version = args.version
 
     for variant in ['']:
         filename = parser.prog.replace(".py","")+args.version+variant
